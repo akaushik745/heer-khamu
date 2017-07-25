5bf8a3847cae2a91da0207b4516a245e3a081be9
akaushik745 <akaushik745@gmail.com>
2017-07-25 18:32:47 +0530

    25th july latest updated

--- kulaBoard/build1.sh
+++
@@ -1,9 +0,0 @@
-
-mkdir serenity
-
-mvn verify  -Dwebdriver.driver=chrome -Denv=staging -Pb1
-mvn serenity:aggregate -Pserenity
-mv  target/site/serenity serenity/firefox
-mv  serenity target/site/serenity
-
-echo done
